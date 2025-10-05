17c2eb62b038677d32fcfa57a32834dca690f2c3
haimhubara <haimhobara@gmail.com>
2025-10-05 14:29:37 +0300

    Add guest script

---
+++ src/scripts/seed_guest.py
@@ -0,0 +1,39 @@
+from models import Base, Users
+from database import  engine
+from passlib.context import CryptContext
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DATABASE_URL =  os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base.metadata.create_all(engine)
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+guest_email = "guest@gmail.com"
+guest_name = "Guest User"
+guest_password = "12345678"
+
+user = session.query(Users).filter(Users.email == guest_email).first()
+if not user:
+    new_user = Users(
+        email=guest_email,
+        name=guest_name,
+        hashed_password=bcrypt_context.hash(guest_password)
+    )
+    session.add(new_user)
+    session.commit()
+    print("Guest user created successfully!")
+else:
+    print("Guest user already exists.")
+
+session.close()

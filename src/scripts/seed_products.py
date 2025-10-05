e667c8e4d443f21b33f671d3814825175e5cbd75
haimhubara <haimhobara@gmail.com>
2025-10-05 17:22:35 +0300

    fix products script

--- src/scripts/seed_products.py
+++ src/scripts/seed_products.py
@@ -25,20 +25,22 @@ with open(json_path, "r", encoding="utf-8") as f:
 products_list = data["products"]  
 
 for p in products_list:
-    product = Products(
-        id=p["id"],
-        name=p["name"],
-        overview=p.get("overview"),
-        long_description=p.get("long_description"),
-        price=p["price"],
-        poster=p.get("poster"),
-        image_local=p.get("image_local"),
-        rating=p.get("rating"),
-        in_stock=p.get("in_stock", True),
-        size=p.get("size"),
-        best_seller=p.get("best_seller", False)
-    )
-    session.add(product)
+    exists = session.query(Products).filter_by(id=p["id"]).first()
+    if not exists:
+        product = Products(
+            id=p["id"],
+            name=p["name"],
+            overview=p.get("overview"),
+            long_description=p.get("long_description"),
+            price=p["price"],
+            poster=p.get("poster"),
+            image_local=p.get("image_local"),
+            rating=p.get("rating"),
+            in_stock=p.get("in_stock", True),
+            size=p.get("size"),
+            best_seller=p.get("best_seller", False)
+        )
+        session.add(product)
 
 session.commit()
 print("All products added successfully!")
